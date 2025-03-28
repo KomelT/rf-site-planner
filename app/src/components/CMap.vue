682691d7c1a995afd4532c6cae1f51892caaa182
Tilen Komel <me@komelt.dev>
2025-03-28 19:23:52 +0100

    Add header

---
+++ app/src/components/CMap.vue
@@ -0,0 +1,22 @@
+<template>
+  <MglMap :mapStyle="mapStyle" :center="mapDefaultCenter" :zoom="mapDefaultZoom">
+    <MglNavigationControl position="top-left" />
+  </MglMap>
+</template>
+<script setup lang="ts">
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { MglMap, MglNavigationControl } from '@indoorequal/vue-maplibre-gl';
+
+const mapStyle =
+  "https://raw.githubusercontent.com/hc-oss/maplibre-gl-styles/master/styles/osm-mapnik/v8/default.json";
+const mapDefaultCenter = [13.75, 45.84] as [
+  number,
+  number,
+];
+const mapDefaultZoom = 12;
+</script>
+<style>
+.maplibregl-map {
+  height: calc(100vh - 80px) !important;
+}
+</style>
---
+++ app/src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="bg-stone-800">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="">
+        <a href="/" class="flex flex-row items-center lg:flex-1 -m-1.5 p-1.5" style="text-decoration: none;">
+          <img class="h-8 w-auto mr-2" src="/meshnet-logo-light.svg" alt="" />
+          <span class="text-gray-200 text-lg font-bold">RF Site Planner</span>
+        </a>
+      </div>
+      <div class="flex lg:hidden">
+        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="size-6" aria-hidden="true" />
+        </button>
+      </div>
+      <div class="hidden lg:flex lg:gap-x-12">
+        <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm/6 font-semibold text-gray-900">{{ item.name }}</a>
+      </div>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <a href="https://github.com/KomelT/rf-site-planner" target="_blank" class="text-xll font-semibold text-gray-900"><GitHubIcon /></a>
+      </div>
+    </nav>
+    <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+      <div class="fixed inset-0 z-10" />
+      <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-stone-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Your Company</span>
+            <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          </a>
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+            <span class="sr-only">Close menu</span>
+            <XMarkIcon class="size-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+            </div>
+            <div class="py-6">
+              <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                <GitHubIcon />
+              </a>
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </Dialog>
+  </header>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Dialog, DialogPanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+import GitHubIcon from '../assets/GitHubIcon.vue'
+
+// { name: 'Product', href: '#' }
+const navigation = []
+
+const mobileMenuOpen = ref(false)
+</script>

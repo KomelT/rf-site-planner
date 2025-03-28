4b8832f848baf92a207fea9e37a72d32336e1223
Tilen Komel <me@komelt.dev>
2025-03-28 19:23:19 +0100

    Add tailwind

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,10 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), tailwindcss()],
 	server: {
 		port: 8080,
 		host: "0.0.0.0",

08592241be55640f377ce84d7995ee2b4beabd8b
Tilen Komel <me@komelt.dev>
2025-03-30 10:28:36 +0200

    Created mode wrapper and update selector

--- app/src/components/ModeSelector/ModeSelector.vue
+++ app/src/components/ModeSelector/ModeSelector.vue
@@ -1,28 +1,44 @@
 <template>
-  <div v-if="showSelector">
-    <p class="text-lg font-bold py-2">Select mode:</p>
-    <div class="grid grid-cols-2 gap-2">
-      <Mode v-for="(mode, index) in modes" :key="index" :title="mode.title" :description="mode.description"
-        :image="mode.image" :onClick="mode.onClick" />
+  <div>
+    <div v-if="showSelector">
+      <p class="text-lg font-bold py-2">Select mode:</p>
+      <div class="grid grid-cols-2 gap-2">
+        <Mode v-for="(mode, index) in modes" :key="index" :title="mode.title" :description="mode.description"
+          :image="mode.image" @click="mode.onClick" />
+      </div>
     </div>
+    <ModeWrapper v-else :title="selectedModeText" @backButtonClick="showSelector = false">
+      <CoverageSimulatorMode />
+    </ModeWrapper>
   </div>
 </template>
 <script setup lang="ts">
 import { computed, ref } from "vue";
+import CoverageSimulatorMode from "../Modes/CoverageSimulatorMode.vue";
+import ModeWrapper from "../Modes/ModeWrapper.vue";
 import Mode from "./Mode.vue";
 
 const showCoverageSim = ref(false);
 
-const showSelector = computed(() => {
-  return !(showCoverageSim.value);
+const selectedModeText = ref("");
+const showSelector = computed({
+	get: () => {
+		return !showCoverageSim.value;
+	},
+	set: (value: boolean) => {
+		showCoverageSim.value = value;
+	},
 });
 
 const modes = [
-  {
-    title: "Coverage simulator:",
-    description: "Simulate coverage of a mesh network",
-    image: import("../../assets/coverage-simulator.jpg"),
-    onClick: () => { showCoverageSim.value = true },
-  },
+	{
+		title: "Coverage simulator:",
+		description: "Simulate coverage of a mesh network",
+		image: import("../../assets/coverage-simulator.jpg"),
+		onClick: () => {
+			showCoverageSim.value = true;
+			selectedModeText.value = "Coverage simulator";
+		},
+	},
 ];
 </script>
---
+++ app/src/components/Modes/ModeWrapper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="flex items-center gap-2 text-lg font-bold pt-2 mb-5 cursor-pointer" @click="$emit('backButtonClick')">
+      <ArrowLeftIcon class="size-6" />
+      {{ props.title }}
+    </div>
+    <slot />
+  </div>
+</template>
+<script setup lang="ts">
+import { ArrowLeftIcon } from "@heroicons/vue/24/outline";
+
+export type ModeWrapperProps = {
+	title: string;
+};
+
+const props = defineProps<ModeWrapperProps>();
+</script>

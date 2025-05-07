479e15bddda9a83f63045fcd95dbd92bfa6de0b3
Tilen Komel <me@komelt.dev>
2025-05-07 13:05:01 +0200

    Add more vertical space to sidebar

--- app/src/components/ModeSelector/ModeSelector.vue
+++ app/src/components/ModeSelector/ModeSelector.vue
@@ -1,7 +1,14 @@
 <template>
 	<div>
 		<div v-if="!anySimTrue">
-			<p class="text-lg font-bold py-2">Select mode:</p>
+			<div class="flex items-center justify-between">
+				<p class="text-lg font-bold py-2">Select mode:</p>
+				<button type="button" class="cursor-pointer -m-2.5 rounded-md p-2.5 text-gray-700"
+					@click="store.mobileMenuOpen = false">
+					<span class="sr-only">Close menu</span>
+					<XMarkIcon class="size-6 text-gray-200" aria-hidden="true" />
+				</button>
+			</div>
 			<div class="grid grid-cols-2 gap-2">
 				<Mode v-for="(mode, index) in modes" :key="index" :title="mode.title" :description="mode.description"
 					:image="mode.image" @click="mode.onClick" />
@@ -14,11 +21,15 @@
 	</div>
 </template>
 <script setup lang="ts">
+import { XMarkIcon } from "@heroicons/vue/24/outline";
 import { computed, ref } from "vue";
+import { useStore } from "../../stores/store";
 import CoverageSimulatorMode from "../Modes/CoverageSimulatorMode.vue";
 import LosSimulatorMode from "../Modes/LosSimulatorMode.vue";
 import ModeWrapper from "../Modes/ModeWrapper.vue";
 import Mode from "./Mode.vue";
+
+const store = useStore();
 
 const showSim = ref({
 	showCoverageSim: false,

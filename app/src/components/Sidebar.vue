6f7f4fc2461f69e35aec64adaa5d4fb4eb81ce6b
Tilen Komel <me@komelt.dev>
2025-05-07 13:09:21 +0200

    Make sidebar scrollable

--- app/src/components/Sidebar.vue
+++ app/src/components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="store.mobileMenuOpen ? 'block' : 'hidden'">
-    <div class="inset-y-0 sm:w-96 h-full overflow-y-auto bg-stone-800 px-4 pb-5 w-screen sm:ring-1 sm:ring-gray-900/10">
+    <div class="inset-y-0 sm:w-96 h-[calc(100vh-64px)] overflow-y-auto bg-stone-800 px-4 pb-5 w-screen sm:ring-1 sm:ring-gray-900/10">
       <div class="mt-6 flow-root">
         <div class="-my-6 divide-y divide-gray-500/10">
           <div>
